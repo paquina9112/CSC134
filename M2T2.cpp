9b104022927cf92ca53a4140eb4aff24b09897db
paquina9112 <paquina9112@student.faytechcc.edu>
2026-02-10 02:07:30 +0000

    prints receipt

--- M2T2.cpp
+++ M2T2.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main() {
     // Declare variables
-    string meal_name = "Burger PLatter";
+    string meal_name = "Burger Platter";
     int num_meals;
     double meal_price = 5.99;
     double sub_total;
@@ -26,14 +26,23 @@ int main() {
     cout << endl;
     cout << "How many would you like" << endl;
     cin >> num_meals;
+    cout << "Tip amount? (min 0)" << endl;
+    cin >> tip_amount;
 
     // Calulate values
     sub_total = num_meals * meal_price;
+    tax_amount = sub_total * tax_rate;
+    total_price = sub_total + tax_amount + tip_amount;
 
     //Print results
-    cout << "YOUR ORDER" << endl << "------------------" << endl;
-    cout << num_meals << " x " << "$" << meal_price << endl;
-    cout << "Subtotal: $" << sub_total << endl;
+    cout << "YOUR ORDER" << endl << "-------------------------------" << endl;
+    cout << num_meals << " x " << meal_name << "\t$" << meal_price << endl;
+    cout << "Subtotal: \t\t$" << sub_total << endl;
+    cout << "Tip: \t\t\t$" << tip_amount << endl;
+    cout << "Tax: \t\t\t$" << tax_amount << endl;
+    cout << "-------------------------------" << endl;
+    cout << "Total: \t\t\t$" << total_price << endl;
+    cout << "THANK YOU COME AGAIN!" << endl;
 
     return 0;
 }
