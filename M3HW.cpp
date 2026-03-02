6f06881be5dcfd95e66675baead552f4721e369d
paquina9112 <paquina9112@student.faytechcc.edu>
2026-03-02 22:03:13 +0000

    started m3hw

---
+++ M3HW.cpp
@@ -0,0 +1,30 @@
+/*
+CSC-134
+M3HW - Gold
+Alexis Paquin
+3/2/2026
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    // Question 1 - 
+    cout << "Question 1:" << endl;
+
+
+    // Question 2 - 
+    cout << "Question 2:" << endl;
+
+
+    // Question 3 - 
+    cout << "Question 3:" << endl;
+
+
+    // Question 4 - 
+    cout << "Question 4:" << endl;
+
+    return 0;
+}
