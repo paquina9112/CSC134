2a9c48b684f4dec1e470f515f815b8bdbd603234
paquina9112 <paquina9112@student.faytechcc.edu>
2026-03-06 22:08:58 +0000

    Question 1 completed

--- M3HW.cpp
+++ M3HW.cpp
@@ -14,17 +14,37 @@ int main() {
     // Question 1 - 
     cout << "Question 1:" << endl;
 
+    string user_ans;
+
+    cout << "Hello, I’m a C++ program!" << endl;
+    cout << "Do you like me? Please type yes or no." << endl;
+    cin >> user_ans;
+
+    if (user_ans == "yes") {
+        cout << "That’s great! I’m sure we’ll get along." << endl;
+    }
+    else if (user_ans == "no") {
+        cout << "Well, maybe you’ll learn to like me later." << endl;
+    }
+    else {
+        cout << "If you’re not sure… that’s OK." << endl;
+    }
+
 
     // Question 2 - 
-    cout << "Question 2:" << endl;
+    cout << "\nQuestion 2:" << endl;
+
 
 
     // Question 3 - 
-    cout << "Question 3:" << endl;
+    cout << "\nQuestion 3:" << endl;
+
 
 
     // Question 4 - 
-    cout << "Question 4:" << endl;
+    cout << "\nQuestion 4:" << endl;
+
+
 
     return 0;
 }
