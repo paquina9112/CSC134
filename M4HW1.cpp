33322947eb791e4b95663148917ae50ab8625e24
paquina9112 <paquina9112@student.faytechcc.edu>
2026-03-23 23:54:13 +0000

    silver finished

--- M4HW1.cpp
+++ M4HW1.cpp
@@ -13,11 +13,14 @@ int main() {
 
     // Declare variables
     int first_num, answer;
-    first_num = 5;
     // second_num = 5;
     // answer = first_num * second_num;
 
+    cout << "Enter a number between 1 and 12:";
+    cin >> first_num;
+
     for ( int i=1; i<=12; i++ ) {
+        
         answer = first_num * i;
         cout << first_num << " times " << i << " is " << answer << endl;
 
