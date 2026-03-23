8befa58dfca5250c8d1b6647e84f08eb3af5e239
paquina9112 <paquina9112@student.faytechcc.edu>
2026-03-23 23:44:19 +0000

    started for loop

---
+++ M4HW1.cpp
@@ -0,0 +1,26 @@
+/*
+CSC - 134
+M4HW1 - 
+Alexis Paquin
+3/22/2026
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Declare variables
+    int first_num, second_num, answer;
+    first_num = 3;
+    second_num = 5;
+    answer = first_num * second_num;
+
+    for ( int i=1; i<=5; i++ ) {
+        cout << first_num << " times " << i << " is " << answer << endl;
+
+
+    }
+
+}
+
