f4e7f8d38bb8823e39b48b8e146b24c57d5a10a5
paquina9112 <paquina9112@student.faytechcc.edu>
2026-03-23 23:51:28 +0000

    Bronze done

--- M4HW1.cpp
+++ M4HW1.cpp
@@ -3,6 +3,7 @@ CSC - 134
 M4HW1 - 
 Alexis Paquin
 3/22/2026
+Multipliction table
 */
 
 #include <iostream>
@@ -11,14 +12,14 @@ using namespace std;
 int main() {
 
     // Declare variables
-    int first_num, second_num, answer;
-    first_num = 3;
-    second_num = 5;
-    answer = first_num * second_num;
+    int first_num, answer;
+    first_num = 5;
+    // second_num = 5;
+    // answer = first_num * second_num;
 
-    for ( int i=1; i<=5; i++ ) {
+    for ( int i=1; i<=12; i++ ) {
+        answer = first_num * i;
         cout << first_num << " times " << i << " is " << answer << endl;
-
 
     }
 
