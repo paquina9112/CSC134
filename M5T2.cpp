8b2ea7ad77c49a5e951b013ee0ad156671a4d97f
paquina9112 <paquina9112@student.faytechcc.edu>
2026-03-31 01:07:59 +0000

    M5T2 done

--- M5T2.cpp
+++ M5T2.cpp
@@ -8,17 +8,27 @@ Alexis Paquin
 #include <iostream>
 using namespace std;
 
+void printResult(int number, int result) {
+    cout << number << " squared = " << result << endl;
+}
+
+int square(int number) {
+    int result;
+    result = number * number;
+    return result;
+}
+
 int main() {
+    // count numbers
+    // find result for each
+    int count = 1;
+    int result;
 
-    int first, second;
-    int count = 0;
-
-    while (count < 10) {
-        first = count;
-        second = 2 * first;
-        cout << first << " " << second << endl;
-        count = count + 1;
-
+    // this will be squares
+    while (count <= 10) {
+        result = square(count);
+        printResult(count, result);
+        count++;
     }
 
 
