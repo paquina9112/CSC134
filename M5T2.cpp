e95cfed3856b00fbc705f59a5644607aec41e924
paquina9112 <paquina9112@student.faytechcc.edu>
2026-03-31 00:37:17 +0000

    started M5T2

---
+++ M5T2.cpp
@@ -0,0 +1,25 @@
+/*
+CSC - 134
+M5T2
+Alexis Paquin
+3/30/2026
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int first, second;
+    int count = 0;
+
+    while (count < 10) {
+        first = count;
+        second = 2 * first;
+        cout << first << " " << second << endl;
+        count = count + 1;
+
+    }
+
+
+}
