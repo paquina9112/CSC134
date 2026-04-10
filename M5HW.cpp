99d91a13ddb8bb1cc0469caac762e27e6f034a81
paquina9112 <paquina9112@student.faytechcc.edu>
2026-04-10 23:52:12 +0000

    Q4 done

--- M5HW.cpp
+++ M5HW.cpp
@@ -1,6 +1,6 @@
 /*
 CSC - 134
-M5HW
+M5HW - Silver
 Alexis Paquin
 4/9/2026
 */
@@ -99,7 +99,45 @@ int main() {
     // Question 4 - Geometry calculator
     cout << "\nQuestion 4:" << endl;
 
-    
+    const double pi = 3.14159;
+    int choice;
+    double radius, circleArea;
+    double rectLength, rectWidth, rectArea;
+    double triBase, triHeight, triArea;
+
+    cout << "Geometry Calculator\n";
+    cout << "1. Calculate the Area of a Circle\n";
+    cout << "2. Calculate the Area of a Rectangle\n";
+    cout << "3. Calculate the Area of a Triangle\n";
+    cout << "4. Quit\n";
+    cout << "Enter your choice (1-4): ";
+    cin >> choice;
+
+    if (choice == 1) {
+        cout << "Enter the radius of the circle: ";
+        cin >> radius;
+        circleArea = pi * (radius * radius);
+        cout << "The area of the circle is " << circleArea << endl;
+    } else if (choice == 2) {
+        cout << "Enter the length of the rectangle: ";
+        cin >> rectLength;
+        cout << "Enter the width of the rectangle: ";
+        cin >> rectWidth;
+        rectArea = rectLength * rectWidth;
+        cout << "The area of the rectangle is " << rectArea << endl;
+    } else if (choice == 3) {
+        cout << "Enter the base of the triangle: ";
+        cin >> triBase;
+        cout << "Enter the height of the triangle: ";
+        cin >> triHeight;
+        triArea = triBase * triHeight * 0.5;
+        cout << "The area of the triangle is " << triArea << endl;
+    } else if (choice == 4) {
+        cout << "Ending program." << endl;
+    } else {
+        cout << "Invalid choice. Please enter a number between 1 and 4." << endl;
+    }
+
 
     // Question 5 - 
     cout << "\nQuestion 5:" << endl;
