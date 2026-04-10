41f8928eebee9833a4f330570c3ed375996efff9
paquina9112 <paquina9112@student.faytechcc.edu>
2026-04-10 03:40:01 +0000

    Finshed Q1

---
+++ M5HW.cpp
@@ -0,0 +1,55 @@
+/*
+CSC - 134
+M5HW
+Alexis Paquin
+4/9/2026
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Question 1 - average rainfall for 3 months
+    cout << "Question 1:" << endl;
+
+    // Variables
+    string month1, month2, month3;
+    double month1num, month2num, month3num, average;
+
+    // Input and output
+    cout << "Enter month: ";
+    cin >> month1;
+    cout << "Enter rainfall for " << month1 << ": ";
+    cin >> month1num;
+    cout << "Enter month: ";
+    cin >> month2;
+    cout << "Enter rainfall for " << month2 << ": ";
+    cin >> month2num;
+    cout << "Enter month: ";
+    cin >> month3;
+    cout << "Enter rainfall for " << month3 << ": ";
+    cin >> month3num;
+    cout << "The average rainfall for " << month1 << ", " << month2 << ", and " << month3 << " is " << (month1num + month2num + month3num) / 3 << "inches" << endl;
+
+    // Question 2 - 
+    cout << "\nQuestion 2:" << endl;
+
+
+    // Question 3 - 
+    cout << "\nQuestion 3:" << endl;
+
+
+    // Question 4 - 
+    cout << "\nQuestion 4:" << endl;
+
+
+    // Question 5 - 
+    cout << "\nQuestion 5:" << endl;
+
+
+    // Question 6 - 
+    cout << "\nQuestion 6:" << endl;
+
+}
+
