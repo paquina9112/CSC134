f0d6c377489d74885e35de2cec0d622f122b5289
paquina9112 <paquina9112@student.faytechcc.edu>
2026-04-10 03:58:54 +0000

    Finished Q2 and Q3

--- M5HW.cpp
+++ M5HW.cpp
@@ -30,15 +30,70 @@ int main() {
     cin >> month3;
     cout << "Enter rainfall for " << month3 << ": ";
     cin >> month3num;
-    cout << "The average rainfall for " << month1 << ", " << month2 << ", and " << month3 << " is " << (month1num + month2num + month3num) / 3 << "inches" << endl;
+    cout << "The average rainfall for " << month1 << ", " << month2 << ", and " << month3 << " is " << (month1num + month2num + month3num) / 3 << " inches" << endl;
 
-    // Question 2 - 
+
+    // Question 2 - Volume of block
     cout << "\nQuestion 2:" << endl;
 
+    // Variable
+    double length, width, height;
 
-    // Question 3 - 
+    // Input and output
+    cout << "Enter length of block: ";
+    cin >> length;
+    cout << "Enter width of block: ";
+    cin >> width;
+    cout << "Enter height of block: ";
+    cin >> height;
+    cout << "The volume of the block is " << length * width * height << " cubic units." << endl;
+
+
+    // Question 3 - number to roman numerals
     cout << "\nQuestion 3:" << endl;
 
+    // Variable
+    int number;
+
+    // Input
+    cout << "Enter a number (1-10): ";
+    cin >> number;
+
+    // Switch statement
+    switch (number) {
+        case 1:
+            cout << "I" << endl;
+            break;
+        case 2:
+            cout << "II" << endl;
+            break;
+        case 3:
+            cout << "III" << endl;
+            break;
+        case 4:
+            cout << "IV" << endl;
+            break;
+        case 5:
+            cout << "V" << endl;
+            break;
+        case 6:
+            cout << "VI" << endl;
+            break;
+        case 7:
+            cout << "VII" << endl;
+            break;
+        case 8:
+            cout << "VIII" << endl;
+            break;
+        case 9:
+            cout << "IX" << endl;
+            break;
+        case 10:
+            cout << "X" << endl;
+            break;
+        default:
+            cout << "Invalid input." << endl;
+    }
 
     // Question 4 - 
     cout << "\nQuestion 4:" << endl;
